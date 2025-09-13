91696bc0bfa5c308881fb2004809ab57a5ea6580
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-13 22:02:36 +0200

    inittial project setup

---
+++ backend/vaultops/src/main/java/com/vaultops/VaultopsApplication.java
@@ -0,0 +1,13 @@
+package com.vaultops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultopsApplication.class, args);
+	}
+
+}
---
+++ backend/vaultops/src/test/java/com/vaultops/VaultopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaultops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
