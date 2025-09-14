dbc46af50a63d40d41b5dee885bc4c69d5b3361a
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-14 20:28:45 +0200

    Update AssetController to handle search asset by name endpoint

--- backend/vaultops/src/main/java/com/vaultops/controllers/AssetController.java
+++ backend/vaultops/src/main/java/com/vaultops/controllers/AssetController.java
@@ -2,9 +2,8 @@ package com.vaultops.controllers;
 
 import com.vaultops.dtos.AssetDTO;
 import com.vaultops.model.Asset;
-import com.vaultops.services.CreateAssetService;
-import com.vaultops.services.GetAssetService;
-import com.vaultops.services.GetAssetsService;
+import com.vaultops.model.UpdateAssetCommand;
+import com.vaultops.services.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +14,19 @@ public class AssetController {
     private final CreateAssetService createAssetService;
     private final GetAssetService getAssetService;
     private final GetAssetsService getAssetsService;
+    private final UpdateAssetService updateAssetService;
+    private final SearchAssetService searchAssetService;
 
     public AssetController(CreateAssetService createAssetService,
                            GetAssetService getAssetService,
-                           GetAssetsService getAssetsService) {
+                           GetAssetsService getAssetsService,
+                           UpdateAssetService updateAssetService,
+                           SearchAssetService searchAssetService) {
         this.createAssetService = createAssetService;
         this.getAssetService = getAssetService;
         this.getAssetsService = getAssetsService;
+        this.updateAssetService = updateAssetService;
+        this.searchAssetService = searchAssetService;
     }
 
     @PostMapping("/asset")
@@ -37,5 +42,15 @@ public class AssetController {
     @GetMapping("/tasks")
     public ResponseEntity<List<AssetDTO>> getAssets() {
         return getAssetsService.execute(null);
+    }
+
+    @GetMapping("/task/search")
+    public ResponseEntity<List<AssetDTO>> searchAssetByName(@RequestParam String name) {
+        return searchAssetService.execute(name);
+    }
+
+    @PutMapping("/asset{id}")
+    public ResponseEntity<AssetDTO> updateAsset(@PathVariable Long id, @RequestBody Asset asset) {
+        return updateAssetService.execute(new UpdateAssetCommand(id, asset));
     }
 }
