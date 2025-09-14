f0e2015e76728635eb3c0c63c5c97fea240c79e1
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-14 17:24:23 +0200

    Update AssetController to handle GET Asset by id endpoint

--- backend/vaultops/src/main/java/com/vaultops/controllers/AssetController.java
+++ backend/vaultops/src/main/java/com/vaultops/controllers/AssetController.java
@@ -3,21 +3,27 @@ package com.vaultops.controllers;
 import com.vaultops.dtos.AssetDTO;
 import com.vaultops.model.Asset;
 import com.vaultops.services.CreateAssetService;
+import com.vaultops.services.GetAssetService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AssetController {
     private final CreateAssetService createAssetService;
+    private final GetAssetService getAssetService;
 
-    public AssetController(CreateAssetService createAssetService) {
+    public AssetController(CreateAssetService createAssetService, GetAssetService getAssetService) {
         this.createAssetService = createAssetService;
+        this.getAssetService = getAssetService;
     }
 
-    @PostMapping
+    @PostMapping("/asset")
     public ResponseEntity<AssetDTO> createAsset(@RequestBody Asset asset) {
         return createAssetService.execute(asset);
+    }
+
+    @GetMapping("task{id}")
+    public ResponseEntity<AssetDTO> getAssetById(@PathVariable Long id) {
+        return getAssetService.execute(id);
     }
 }
