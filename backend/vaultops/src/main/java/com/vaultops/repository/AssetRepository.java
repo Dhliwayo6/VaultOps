7baa7205191a2e92fce8bdbb973dba0f91812f47
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-14 20:22:32 +0200

    Update asset repository to cater for search asset by name endpoint

--- backend/vaultops/src/main/java/com/vaultops/repository/AssetRepository.java
+++ backend/vaultops/src/main/java/com/vaultops/repository/AssetRepository.java
@@ -2,6 +2,15 @@ package com.vaultops.repository;
 
 import com.vaultops.model.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+
+    List<Asset> findByNameContaining(String name);
+
+    @Query("SELECT a FROM Asset a WHERE a.name LIKE %:keyword% OR a.type LIKE %:keyword%")
+    List<Asset> findByNameOrTypeContaining(@Param("keyword") String name);
 }
