a6c10150a64fb6a8c78c6f13134828bcb3abb8ee
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-14 17:19:35 +0200

    Add GetAssetService to project

---
+++ backend/vaultops/src/main/java/com/vaultops/services/GetAssetService.java
@@ -0,0 +1,29 @@
+package com.vaultops.services;
+
+import com.vaultops.Query;
+import com.vaultops.dtos.AssetDTO;
+import com.vaultops.model.Asset;
+import com.vaultops.repository.AssetRepository;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+public class GetAssetService implements Query<Long, AssetDTO> {
+
+    private AssetRepository assetRepository;
+
+    public GetAssetService(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
+
+    @Override
+    public ResponseEntity<AssetDTO> execute(Long id) {
+        Optional<Asset> assetOptional = assetRepository.findById(id);
+
+        if (assetOptional.isPresent()) {
+            return ResponseEntity.ok(new AssetDTO(assetOptional.get()));
+        }
+
+        throw new IllegalArgumentException("Task not found");
+    }
+}
