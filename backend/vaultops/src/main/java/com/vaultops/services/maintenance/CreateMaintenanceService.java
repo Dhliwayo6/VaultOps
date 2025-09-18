fb70da0aeac75c3d341308eebaa626a0dbd73867
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-18 23:10:45 +0200

    Add create maintenance service class

---
+++ backend/vaultops/src/main/java/com/vaultops/services/maintenance/CreateMaintenanceService.java
@@ -0,0 +1,26 @@
+package com.vaultops.services.maintenance;
+
+
+import com.vaultops.Command;
+import com.vaultops.dtos.MaintenanceDTO;
+import com.vaultops.model.Maintenance;
+import com.vaultops.repository.MaintenanceRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateMaintenanceService implements Command<Maintenance, MaintenanceDTO> {
+
+    private MaintenanceRepository maintenanceRepository;
+
+    public CreateMaintenanceService(MaintenanceRepository maintenanceRepository) {
+        this.maintenanceRepository = maintenanceRepository;
+    }
+
+    @Override
+    public ResponseEntity<MaintenanceDTO> execute(Maintenance maintenance) {
+        Maintenance maintenanceSaved = maintenanceRepository.save(maintenance);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new MaintenanceDTO(maintenanceSaved));
+    }
+}
