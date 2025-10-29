f9ee99e1f719fbb02fc9db775fc36a1256fb2896
Alexander-Agu <firstsonagu@gmail.com>
2025-10-29 11:12:25 +0200

    Initializing the frontend project

---
+++ Client/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import "./sidebar.css";
+import { MdDashboard } from "react-icons/md";
+import { MdOutlineInventory2 } from "react-icons/md";
+
+export default function Sidebar() {
+  return (
+    <nav className="dashboard-nav">
+        <div>
+            <a href="#">WTC</a>
+        </div>
+        
+
+        <ul className="dash-links">
+            <li>
+                <a href="#">
+                    <MdDashboard />
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <MdOutlineInventory2 />
+                    <p>Inventory</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <MdDashboard />
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <MdDashboard />
+                    <p>Dashboard</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
