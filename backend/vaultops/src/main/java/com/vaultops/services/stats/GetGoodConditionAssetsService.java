ee8354c86621324f1833ea8f18d4dab6918d1219
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-10 23:58:53 +0200

    add service class to track assets in good condition

---
+++ backend/vaultops/src/main/java/com/vaultops/services/stats/GetGoodConditionAssetsService.java
@@ -0,0 +1,22 @@
+package com.vaultops.services.stats;
+
+import com.vaultops.Query;
+import com.vaultops.enums.ConditionStatus;
+import com.vaultops.repository.AssetRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetGoodConditionAssetsService implements Query<Void, Integer> {
+    private final AssetRepository assetRepository;
+
+    public GetGoodConditionAssetsService(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
+
+    @Override
+    public ResponseEntity<Integer> execute(Void input) {
+        Integer count = assetRepository.countAssetsByConditionStatus(ConditionStatus.GOOD);
+        return ResponseEntity.ok(count);
+    }
+}
