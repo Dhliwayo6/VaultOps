fc1576d867b2670012ac04a8513022ee90f6572b
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-10 23:12:10 +0200

    Initialize service class to get assets in use

---
+++ backend/vaultops/src/main/java/com/vaultops/services/stats/GetAssetsInUseService.java
@@ -0,0 +1,12 @@
+package com.vaultops.services.stats;
+
+import com.vaultops.Query;
+import org.springframework.http.ResponseEntity;
+
+public class GetAssetsInUseService implements Query {
+
+    @Override
+    public ResponseEntity execute(Object input) {
+        return null;
+    }
+}
