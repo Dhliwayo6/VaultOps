ea2d4a5eb9acda5715672e16b6311829955e5e32
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-10 23:54:00 +0200

    add service class to track damaged assets

---
+++ backend/vaultops/src/main/java/com/vaultops/services/stats/GetDamagedConditionAssetsService.java
@@ -0,0 +1,22 @@
+package com.vaultops.services.stats;
+
+import com.vaultops.Query;
+import com.vaultops.enums.ConditionStatus;
+import com.vaultops.repository.AssetRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetDamagedConditionAssetsService implements Query<Void, Integer>{
+    private final AssetRepository assetRepository;
+
+    public GetDamagedConditionAssetsService(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
+
+    @Override
+    public ResponseEntity<Integer> execute(Void input) {
+        Integer count = assetRepository.countAssetsByConditionStatus(ConditionStatus.DAMAGED);
+        return ResponseEntity.ok(count);
+    }
+}
