6157afb4420b74217a892e386187a348ff43a3ac
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-11 23:51:04 +0200

    Add usage endpoints to StatsController

--- backend/vaultops/src/main/java/com/vaultops/controllers/StatsController.java
+++ backend/vaultops/src/main/java/com/vaultops/controllers/StatsController.java
@@ -1,4 +1,52 @@
 package com.vaultops.controllers;
 
+import com.vaultops.services.stats.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StatsController {
+
+    private final GetAssetsInUseService getAssetsInUseService;
+    private final GetAssetsInStorageService getAssetsInStorageService;
+    private final GetAssetsInRepairsService getAssetsInRepairsService;
+    private final GetExcellentConditionAssetsService getExcellentConditionAssetsService;
+    private final GetGoodConditionAssetsService getGoodConditionAssetsService;
+    private final GetFairConditionAssetsService getFairConditionAssetsService;
+    private final GetBadConditionAssetsService getBadConditionAssetsService;
+    private final GetDamagedConditionAssetsService getDamagedConditionAssetsService;
+
+    public StatsController(GetAssetsInUseService getAssetsInUseService,
+                           GetAssetsInStorageService getAssetsInStorageService,
+                           GetAssetsInRepairsService getAssetsInRepairsService,
+                           GetExcellentConditionAssetsService getExcellentConditionAssetsService,
+                           GetGoodConditionAssetsService getGoodConditionAssetsService,
+                           GetFairConditionAssetsService getFairConditionAssetsService,
+                           GetBadConditionAssetsService getBadConditionAssetsService,
+                           GetDamagedConditionAssetsService getDamagedConditionAssetsService) {
+        this.getAssetsInUseService = getAssetsInUseService;
+        this.getAssetsInStorageService = getAssetsInStorageService;
+        this.getAssetsInRepairsService = getAssetsInRepairsService;
+        this.getExcellentConditionAssetsService = getExcellentConditionAssetsService;
+        this.getGoodConditionAssetsService = getGoodConditionAssetsService;
+        this.getFairConditionAssetsService = getFairConditionAssetsService;
+        this.getBadConditionAssetsService = getBadConditionAssetsService;
+        this.getDamagedConditionAssetsService = getDamagedConditionAssetsService;
+    }
+
+    @GetMapping("/stats/assets/in-use")
+    public ResponseEntity<Integer> getAssetsInUse() {
+        return getAssetsInUseService.execute(null);
+    }
+
+    @GetMapping("/stats/assets/storage")
+    public ResponseEntity<Integer> getAssetsInStorage() {
+        return getAssetsInStorageService.execute(null);
+    }
+
+    @GetMapping("/stats/assets/in-repasirs")
+    public ResponseEntity<Integer> getAssetsInService() {
+        return getAssetsInRepairsService.execute(null);
+    }
 }
