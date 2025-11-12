5e9756b043824317e2a40c163f2f93d5659f7c84
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-12 20:44:26 +0200

    Update top four assets service class to find find and sort assets in service

--- backend/vaultops/src/main/java/com/vaultops/services/asset/GetTopForAssetsInRepairs.java
+++ backend/vaultops/src/main/java/com/vaultops/services/asset/GetTopForAssetsInRepairs.java
@@ -1,14 +1,32 @@
 package com.vaultops.services.asset;
 
 import com.vaultops.Query;
+import com.vaultops.dtos.AssetDTO;
 import com.vaultops.dtos.AssetDTO2;
+import com.vaultops.enums.Usage;
+import com.vaultops.model.Asset;
+import com.vaultops.repository.AssetRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GetTopForAssetsInRepairs implements Query<Void, List<AssetDTO2>> {
+    private AssetRepository assetRepository;
+
+    public GetTopForAssetsInRepairs(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
+
     @Override
     public ResponseEntity<List<AssetDTO2>> execute(Void input) {
-        return null;
+        List<Asset> assets = assetRepository.findAssetsByUsageStatus(Usage.SERVICE).stream().limit(4).toList();
+        List<AssetDTO2>  assetDTOs = assets.stream()
+                .sorted(Comparator.comparing(Asset::getCreatedAt).reversed())
+                .map(AssetDTO2::new)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(assetDTOs);
     }
 }
