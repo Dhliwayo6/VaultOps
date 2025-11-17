56022c876c7d50e2630e9938ca5f5360267dca4e
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-18 00:26:01 +0200

    Update base test class to include mock servvices

--- backend/vaultops/src/test/java/com/vaultops/assets/AssetControllerTestBase.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/AssetControllerTestBase.java
@@ -1,8 +1,32 @@
 package com.vaultops.assets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaultops.controllers.AssetController;
+import com.vaultops.services.asset.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AssetController.class)
 public abstract class AssetControllerTestBase {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected CreateAssetService createAssetService;
+    @MockitoBean
+    protected GetAssetService getAssetService;
+    @MockitoBean
+    protected GetAssetsService getAssetsService;
+    @MockitoBean
+    protected UpdateAssetService updateAssetService;
+    @MockitoBean
+    protected SearchAssetService searchAssetService;
+    @MockitoBean
+    protected DeleteAssetService deleteAssetService;
 }
