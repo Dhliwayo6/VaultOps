2f5a40c5a2a9a35c7616b902195cee91a7d1aa16
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-17 23:48:38 +0200

    Add base abstract class for Asset controller tests

---
+++ backend/vaultops/src/test/java/com/vaultops/assets/AssetControllerTestBase.java
@@ -0,0 +1,8 @@
+package com.vaultops.assets;
+
+import com.vaultops.controllers.AssetController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(AssetController.class)
+public abstract class AssetControllerTestBase {
+}
