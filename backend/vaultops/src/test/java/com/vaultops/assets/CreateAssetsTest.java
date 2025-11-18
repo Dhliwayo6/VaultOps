d7f8052decf9d8e04ffcd6974d8f056c79a31e78
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-19 01:25:58 +0200

    Update CreateAssetTest with a test create an asset with a valid data

---
+++ backend/vaultops/src/test/java/com/vaultops/assets/CreateAssetsTest.java
@@ -0,0 +1,78 @@
+package com.vaultops.assets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaultops.dtos.AssetDTO;
+import com.vaultops.enums.Assignment;
+import com.vaultops.enums.ConditionStatus;
+import com.vaultops.enums.Usage;
+import com.vaultops.model.Asset;
+import com.vaultops.services.asset.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Tests for Create Asset Endpoint")
+public class CreateAssetsTest {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+    @MockitoBean
+    private CreateAssetService createAssetService;
+    @MockitoBean private GetAssetService getAssetService;
+    @MockitoBean private GetAssetsService getAssetsService;
+    @MockitoBean private DeleteAssetService deleteAssetService;
+    @MockitoBean private UpdateAssetService updateAssetService;
+    @MockitoBean private SearchAssetService searchAssetService;
+
+    private Asset asset;
+    private AssetDTO assetDTO;
+
+    @BeforeEach
+    void setUp() {
+        asset = new Asset();
+        asset.setId(1L);
+        asset.setName("Macbook Laptop");
+        asset.setType("Laptop");
+        asset.setUsageStatus(Usage.IN_USE);
+        asset.setConditionStatus(ConditionStatus.FAIR);
+        asset.setAssignment(Assignment.ASSIGNED);
+
+        assetDTO = new AssetDTO(asset);
+
+    }
+
+    @Test
+    @DisplayName("Create asset successfully and 201 status")
+    void createAsset_ValidData_ShouldReturnCreatedStaus() throws Exception {
+        when(createAssetService.execute(any(Asset.class))).thenReturn(ResponseEntity.status(201).body(assetDTO));
+
+        mockMvc.perform(post("/api/asset")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(asset)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Macbook Laptop"))
+                .andExpect(jsonPath("$.type").value("Laptop"))
+                .andExpect(jsonPath("$.usageStatus").value("IN_USE"))
+                .andExpect(jsonPath("$.conditionStatus").value("FAIR"))
+                .andExpect(jsonPath("$.assignment").value("ASSIGNED"));
+
+        verify(createAssetService, times(1)).execute(any(Asset.class));
+    }
+
+
+}
