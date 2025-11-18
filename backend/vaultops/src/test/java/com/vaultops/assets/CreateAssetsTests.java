a7620461b1b0ba94044563e1b774dcae157c8701
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-18 23:52:41 +0200

    Initialize CreateAssets test class

--- backend/vaultops/src/test/java/com/vaultops/assets/CreateAssetsTests.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/CreateAssetsTests.java
@@ -7,25 +7,33 @@ import com.vaultops.enums.Assignment;
 import com.vaultops.enums.ConditionStatus;
 import com.vaultops.enums.Usage;
 import com.vaultops.model.Asset;
-import com.vaultops.services.asset.CreateAssetService;
+import com.vaultops.services.asset.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AssetController.class)
+@DisplayName("Tests for Create Asset Endpoint")
 public class CreateAssetsTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @MockBean
-    private CreateAssetService createAssetService;
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+    @MockitoBean private CreateAssetService createAssetService;
+    @MockitoBean private GetAssetService getAssetService;
+    @MockitoBean private GetAssetsService getAssetsService;
+    @MockitoBean private DeleteAssetService deleteAssetService;
+    @MockitoBean private UpdateAssetService updateAssetService;
+    @MockitoBean private SearchAssetService searchAssetService;
 
     private Asset asset;
     private AssetDTO assetDTO;
 
+    @BeforeEach
     void setUp() {
         asset = new Asset();
         asset.setId(1L);
@@ -38,5 +46,6 @@ public class CreateAssetsTests {
         assetDTO = new AssetDTO(asset);
 
     }
+
 
 }
