d516f7539d4fb795a4999a873e728ac46fb50f40
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-18 23:29:54 +0200

    Add TestData class for creating data across all tests

---
+++ backend/vaultops/src/test/java/com/vaultops/assets/TestData.java
@@ -0,0 +1,28 @@
+package com.vaultops.assets;
+
+import com.vaultops.enums.Assignment;
+import com.vaultops.enums.ConditionStatus;
+import com.vaultops.enums.Usage;
+import com.vaultops.model.Asset;
+
+import java.time.LocalDate;
+
+public class TestData {
+    public static Asset createInitialAsset() {
+        Asset asset = new Asset();
+        asset.setId(1L);
+        asset.setName("Macbook laptop");
+        asset.setType("Laptop");
+        asset.setUsageStatus(Usage.IN_USE);
+        asset.setConditionStatus(ConditionStatus.FAIR);
+        asset.setAssignment(Assignment.ASSIGNED);
+        asset.setCreatedAt(LocalDate.now());
+        return asset;
+    }
+
+    public static Asset createAssetWithId(Long id) {
+        Asset asset = createInitialAsset();
+        asset.setId(id);
+        return asset;
+    }
+}
