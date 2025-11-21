d8dfe28b62c5b9c2afabf3c8c7da468194b2ec26
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-21 22:06:11 +0200

    Initialize DeleteAssetsTests

---
+++ backend/vaultops/src/test/java/com/vaultops/assets/controllers/DeleteAssetsTests.java
@@ -0,0 +1,31 @@
+package com.vaultops.assets.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaultops.dtos.AssetDTO;
+import com.vaultops.model.Asset;
+import com.vaultops.services.asset.*;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Tests for delete asset endpoint")
+public class DeleteAssetsTests {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired ObjectMapper mapper;
+    @MockitoBean private CreateAssetService createAssetService;
+    @MockitoBean private GetAssetService getAssetService;
+    @MockitoBean private GetAssetsService getAssetsService;
+    @MockitoBean private DeleteAssetService deleteAssetService;
+    @MockitoBean private UpdateAssetService updateAssetService;
+    @MockitoBean private SearchAssetService searchAssetService;
+
+    private Asset asset;
+    private AssetDTO assetDTO;
+
+}
