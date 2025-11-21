dd69cf8746e2ae2d97507bdfc812f42917412eed
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-21 22:18:03 +0200

    DeleteAssetTests - add test for valid delete request

--- backend/vaultops/src/test/java/com/vaultops/assets/controllers/DeleteAssetsTests.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/controllers/DeleteAssetsTests.java
@@ -5,11 +5,18 @@ import com.vaultops.dtos.AssetDTO;
 import com.vaultops.model.Asset;
 import com.vaultops.services.asset.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,7 +32,14 @@ public class DeleteAssetsTests {
     @MockitoBean private UpdateAssetService updateAssetService;
     @MockitoBean private SearchAssetService searchAssetService;
 
-    private Asset asset;
-    private AssetDTO assetDTO;
+    @Test
+    @DisplayName(("Should return 204 NO CONTENT status when asset exists"))
+    void deleteAssetById_WhenAssetIsPresent_ShouldReturnNoContent() throws Exception{
+        when(deleteAssetService.execute(1L)).thenReturn(ResponseEntity.noContent().build());
+
+        mockMvc.perform(delete("/api/asset/1")).andExpect(status().isNoContent()).andExpect(content().string(""));
+        verify(deleteAssetService, times(1)).execute(1L);
+
+    }
 
 }
