844c9e0e87f9dcaf8a21cc241aa7caeba1cd0c60
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-21 23:16:44 +0200

    Add test for returning all assets as a list

--- backend/vaultops/src/test/java/com/vaultops/assets/controllers/GetAllAssetsTests.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/controllers/GetAllAssetsTests.java
@@ -9,13 +9,23 @@ import com.vaultops.model.Asset;
 import com.vaultops.services.asset.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -57,6 +67,22 @@ public class GetAllAssetsTests {
         asset2.setUsageStatus(Usage.SERVICE);
         asset2.setConditionStatus(ConditionStatus.DAMAGED);
         asset2.setAssignment(Assignment.UNASSIGNED);
-        
+
+        assetDTOS = Arrays.asList(new AssetDTO(asset), new AssetDTO(asset1), new AssetDTO(asset2));
+    }
+
+    @Test
+    @DisplayName("Should return list of all assets")
+    void getAllAssets_WhenAssetsArePresent_ReturnList() throws Exception{
+        when(getAssetsService.execute(null)).thenReturn(ResponseEntity.ok(assetDTOS));
+
+        mockMvc.perform(get("/api/assets"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].name").value("Macbook Laptop"))
+                .andExpect(jsonPath("$[1].name").value("MSi Modern 14 Laptop"))
+                .andExpect(jsonPath("$[2].name").value("ViewChoice Monitor"));
+
+        verify(getAssetsService, times(1)).execute(null);
     }
 }
