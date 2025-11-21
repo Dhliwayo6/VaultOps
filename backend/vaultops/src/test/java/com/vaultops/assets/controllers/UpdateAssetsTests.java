a813be476779bac2983e4a3da9faf98e175ec3ee
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-22 00:50:41 +0200

    UpdateAssetsTest - add test for a valid request

--- backend/vaultops/src/test/java/com/vaultops/assets/controllers/UpdateAssetsTests.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/controllers/UpdateAssetsTests.java
@@ -6,6 +6,7 @@ import com.vaultops.enums.Assignment;
 import com.vaultops.enums.ConditionStatus;
 import com.vaultops.enums.Usage;
 import com.vaultops.model.Asset;
+import com.vaultops.model.UpdateAssetCommand;
 import com.vaultops.services.asset.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +14,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -43,5 +52,29 @@ public class UpdateAssetsTests {
 
     }
 
+    @Test
+    @DisplayName("Should update asset successfully")
+    void updateAsset_WhenAssetIsPresent_ShouldReturnUpdatedAsset() throws Exception{
+        Asset updatedAsset = new Asset();
+        updatedAsset.setId(1L);
+        updatedAsset.setName("Macbook Laptop - Updated");
+        updatedAsset.setType("Laptop");
+        updatedAsset.setUsageStatus(Usage.STORAGE);
+        updatedAsset.setConditionStatus(ConditionStatus.FAIR);
+        updatedAsset.setAssignment(Assignment.UNASSIGNED);
 
+        AssetDTO updatedAssetDTO = new AssetDTO(updatedAsset);
+
+        when(updateAssetService.execute(any(UpdateAssetCommand.class))).thenReturn(ResponseEntity.ok(updatedAssetDTO));
+
+        mockMvc.perform(put("/api/asset/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedAssetDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Macbook Laptop - Updated"))
+                .andExpect(jsonPath("$.type").value("Laptop"))
+                .andExpect(jsonPath("$.usageStatus").value("STORAGE"))
+                .andExpect(jsonPath("$.conditionStatus").value("FAIR"))
+                .andExpect(jsonPath("$.assignment").value("UNASSIGNED"));
+    }
 }
