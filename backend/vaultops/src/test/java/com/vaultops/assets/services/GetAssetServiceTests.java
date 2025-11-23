92feb21b8c191a91444d84dc062929299defc315
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-23 23:51:38 +0200

    getAssetServiceTests - Add to to test if service class gets back an asset if it is present

--- backend/vaultops/src/test/java/com/vaultops/assets/services/GetAssetServiceTests.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/services/GetAssetServiceTests.java
@@ -1,23 +1,33 @@
 package com.vaultops.assets.services;
 
+import com.vaultops.dtos.AssetDTO;
 import com.vaultops.enums.Usage;
 import com.vaultops.model.Asset;
 import com.vaultops.repository.AssetRepository;
 import com.vaultops.services.asset.CreateAssetService;
+import com.vaultops.services.asset.GetAssetService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Create Asset Service tests")
 public class GetAssetServiceTests {
     @Mock private AssetRepository assetRepository;
-    @InjectMocks CreateAssetService createAssetService;
+    @InjectMocks GetAssetService getAssetService;
     private Asset asset;
 
     @BeforeEach
@@ -28,5 +38,21 @@ public class GetAssetServiceTests {
         asset.setType("Electronics");
         asset.setUsageStatus(Usage.IN_USE);
         asset.setCreatedAt(LocalDate.now());
+    }
+
+    @Test
+    @DisplayName("Should return asset when found")
+    void execute_WhenAssetExists_ShouldReturnAsset() {
+        when(assetRepository.findById(1L)).thenReturn(Optional.of(asset));
+
+        ResponseEntity<AssetDTO> response = getAssetService.execute(1L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        Assertions.assertNotNull(response.getBody());
+        assertThat(response.getBody().getId()).isEqualTo(1L);
+        assertThat(response.getBody().getName()).isEqualTo("Laptop");
+
+        verify(assetRepository, times(1)).findById(1L);
     }
 }
