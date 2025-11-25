d9d5466af41e208c4d5be95c8110bbc8b2de9f1a
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-25 21:00:00 +0200

    Initialize Delete Assets service tests

---
+++ backend/vaultops/src/test/java/com/vaultops/assets/services/DeleteAssetServiceTest.java
@@ -0,0 +1,32 @@
+package com.vaultops.assets.services;
+
+import com.vaultops.enums.Usage;
+import com.vaultops.model.Asset;
+import com.vaultops.repository.AssetRepository;
+import com.vaultops.services.asset.DeleteAssetService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Delete Asset Service tests")
+public class DeleteAssetServiceTest {
+    @Mock private AssetRepository assetRepository;
+    @InjectMocks DeleteAssetService deleteAssetService;
+    private Asset asset;
+
+    @BeforeEach
+    void setUp() {
+        asset = new Asset();
+        asset.setId(1L);
+        asset.setName("Laptop");
+        asset.setType("Electronics");
+        asset.setUsageStatus(Usage.IN_USE);
+        asset.setCreatedAt(LocalDate.now());
+    }
+}
