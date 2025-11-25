907fe71d574345a7654df65991319c362d88d669
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-11-25 21:26:40 +0200

    DeleteAssetServiceTests - Add test to check if service class deletes asset

--- backend/vaultops/src/test/java/com/vaultops/assets/services/DeleteAssetServiceTest.java
+++ backend/vaultops/src/test/java/com/vaultops/assets/services/DeleteAssetServiceTest.java
@@ -6,12 +6,19 @@ import com.vaultops.repository.AssetRepository;
 import com.vaultops.services.asset.DeleteAssetService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Delete Asset Service tests")
@@ -29,4 +36,20 @@ public class DeleteAssetServiceTest {
         asset.setUsageStatus(Usage.IN_USE);
         asset.setCreatedAt(LocalDate.now());
     }
+
+    @Test
+    @DisplayName("Should delete asset when it exists")
+    void execute_WhenAssetExists_ShouldDeleteAndReturnNotFound() {
+        when(assetRepository.findById(1L)).thenReturn(Optional.of(asset));
+        doNothing().when(assetRepository).deleteById(1L);
+
+        ResponseEntity<Void> response = deleteAssetService.execute(1L);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        assertThat(response.getBody()).isNull();
+
+        verify(assetRepository, times(1)).findById(1L);
+        verify(assetRepository, times(1)).deleteById(1L);
+    }
+
 }
