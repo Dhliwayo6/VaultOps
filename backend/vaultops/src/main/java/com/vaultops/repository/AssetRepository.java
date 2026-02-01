f95edebcec85f078675cdefc5cf73ad2d9f60d8d
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2026-02-01 23:27:26 +0200

    Update Asset repository

--- backend/vaultops/src/main/java/com/vaultops/repository/AssetRepository.java
+++ backend/vaultops/src/main/java/com/vaultops/repository/AssetRepository.java
@@ -4,12 +4,14 @@ import com.vaultops.enums.ConditionStatus;
 import com.vaultops.enums.Usage;
 import com.vaultops.model.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface AssetRepository extends JpaRepository<Asset, Long> {
+public interface AssetRepository extends JpaRepository<Asset, Long>, JpaSpecificationExecutor<Asset> {
 
     List<Asset> findByNameContaining(String name);
 
@@ -20,4 +22,9 @@ public interface AssetRepository extends JpaRepository<Asset, Long> {
     List<Asset> findAssetsByConditionStatus(ConditionStatus condition);
     Long countAssetsByUsageStatus(Usage usage);
     Long countAssetsByConditionStatus(ConditionStatus condition);
+
+    Optional<Asset> findBySerialNumber(String serialNumber);
+    List<Asset> findByType(String type);
+    List<Asset> findByTypeAndConditionStatus(String type, ConditionStatus conditionStatus);
+
 }
