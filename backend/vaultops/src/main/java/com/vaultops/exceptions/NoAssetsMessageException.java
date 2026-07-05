6ff737fae2bef1853fc4df070213d6e9d2b28dbf
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2026-07-06 00:54:35 +0200

    clean up

--- backend/vaultops/src/main/java/com/vaultops/exceptions/NoAssetsMessageException.java
+++
@@ -1,9 +0,0 @@
-package com.vaultops.exceptions;
-
-import com.vaultops.enums.ErrorMessages;
-
-public class NoAssetsMessageException extends RuntimeException{
-    public NoAssetsMessageException() {
-        super(ErrorMessages.NO_ASSETS.getMessage());
-    }
-}
